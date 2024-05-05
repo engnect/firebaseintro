decc0c3449783320d48ed94c11702fee203ed30f
Engin Taşkın <engin48ect@gmail.com>
2024-05-05 23:45:26 +0300

    firebase login signup and auth

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebaseintro/firebase_options.dart';
+import 'package:firebaseintro/screens/auth.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FirebaseApp firebaseApp = await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform);
+
+  // StreamBuilder => FirebaseAuth.AuthStateChanges
+  runApp(const MaterialApp(home: Auth()));
+}
